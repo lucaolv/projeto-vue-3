89684e10d962c4ffd11ad4bcb13c1a8418ea2943
lucaolv <111596218+lucaolv@users.noreply.github.com>
2025-01-16 11:34:18 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="px-3 py-10 md:px-10 bg-gray-900">
+    <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+      <TodoSpinner v-if="loading" />
+
+      <template v-else>
+        <TodoFormAdd />
+
+        <TodoItems />
+
+        <TodoEmpty />
+      </template>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoSpinner from "@/components/TodoSpinner";
+import TodoFormAdd from "@/components/TodoFormAdd";
+import TodoItems from "@/components/TodoItems";
+import TodoEmpty from "@/components/TodoEmpty";
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
+  components: { TodoItems, TodoFormAdd, TodoSpinner, TodoEmpty },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data() {
+    return {
+      loading: false,
+    };
+  },
+
+  created() {
+    this.loading = true;
+    axios
+      .get("http://localhost:3000/todos")
+      .then((response) => {
+        this.$store.commit("storeTodos", response.data);
+      })
+      .finally(() => {
+        this.loading = false;
+      });
+  },
+};
+</script>
